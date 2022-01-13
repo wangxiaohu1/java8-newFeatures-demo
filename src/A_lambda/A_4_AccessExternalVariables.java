b987dbb5a6a787e0b45a04fd56f0ffacca8bb29e
xiaohu.wxh <xiaohu.wxh@alibabapictures.com>
2022-01-13 23:11:08 +0800

    lambda

--- src/A_lambda/A_4_AccessExternalVariables.java
+++ src/A_lambda/A_4_AccessExternalVariables.java
@@ -2,6 +2,8 @@ package A_lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * @author wangxiaohu
@@ -9,13 +11,14 @@ import java.util.List;
  * @Description Lambda 本质上是匿名内部类的改装，所以它使用到的变量都会隐式的转成 final
  */
 public class A_4_AccessExternalVariables {
+    private static String demo="1";
 
     public static void main(String[] args) {
         // Lambda 本质上是匿名内部类的改装，所以它使用到的变量都会隐式的转成 final
         int x1= 2;
         List<Integer> list1 = new ArrayList<>();
         list1.add(1);
-        list1.forEach(e -> System.out.println(e + x1));
+        list1.forEach(e -> System.out.println(e + x1 + "::'" +demo));
 
         // 上面整个等价于
         final int x2= 2;
@@ -23,6 +26,13 @@ public class A_4_AccessExternalVariables {
         list2.add(1);
         list2.forEach(e -> System.out.println(e + x2));
 
+        // 但是不能修改lambda表达式域外的变量，因为域外变量被lambda引用后，变量会被编译器隐式编译成final
+        int x3 = 3;
+        Stream.of(1).forEach(e->{
+            System.out.println(x3-e);
+            //尝试修改x3值，编译报错
+            //x3 = 1;
+        });
+        System.out.println("end");
     }
-
 }
--- src/A_lambda/A_4_AccessInterfaceDefaultMethod.java
+++ src/A_lambda/A_4_AccessInterfaceDefaultMethod.java
@@ -1,4 +1,27 @@
 package A_lambda;
-
+/**
+ * @author wangxiaohu
+ * @version Id: A_4_AccessInterfaceDefaultMethod.java, v0.1 2020年11月10日 15:00:53 wangxiaohu Exp $
+ * @Description Lambda 内部不能访问接口默认方法，但是匿名内部类类可以访问
+ */
 public class A_4_AccessInterfaceDefaultMethod {
+    public static void main(String[] args) {
+        // 内部不能访问接口默认方法，编译报错
+        //Integer2LongFunction fuc = a -> {return defaultToLongNumString(a);};
+
+        // 但是匿名内部类类可以访问
+        Integer2LongFunction func2 = new Integer2LongFunction(){
+            @Override
+            public String toLongNumString(Integer i) {
+                return defaultToLongNumString(i);
+            }
+        };
+    }
+    interface Integer2LongFunction{
+        String toLongNumString(Integer i);
+
+        default String defaultToLongNumString(Integer i){
+            return i.longValue() + "L";
+        }
+    }
 }
